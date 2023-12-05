880d62e2221dd94b217e0c11830c1f08114729ff
yuji-otomo <y_otomo@libertyfish.co.jp>
2023-12-05 18:54:16 +0900

    変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,6 @@ set :deploy_to,       "/home/deploy/rails/#{fetch(:application)}"
 # puma
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_bind,       "unix:///tmp/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
